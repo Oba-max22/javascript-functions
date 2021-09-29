749d27c578f4487038ee19d195aab61d326cbaec
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2021-09-29 20:50:51 +0100

    first commit

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,6 +1,10 @@
-function seed() {}
+function seed() {
+  return [...arguments];
+}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return x === j && y === k ;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {}
